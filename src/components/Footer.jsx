923db1eda403615fc4b07f53dc9721403f01edef
MWyssy <mike@wyss.co.uk>
2023-08-30 20:56:39 +0100

    make footer responsive

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,17 +4,18 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { faInstagram, faFacebook } from '@fortawesome/free-brands-svg-icons'
 
 function Footer() {
+    const emailSubject = 'Photography Enquiry'
 
     return (
         <footer id='footer'>
             <section id='icons'>
-                <a href='' target='_blank' rel='noreferrer'>
+                <a href={`mailto:eliza.medrea@gmail.com?subject=${emailSubject}`} target='_blank' rel='noreferrer'>
                     <FontAwesomeIcon icon={faEnvelope} className='icon' />
                 </a>
                 <a href='' target='_blank' rel='noreferrer'>
                     <FontAwesomeIcon icon={faInstagram} className='icon' /> 
                 </a>
-                <a href='' target='_blank' rel='noreferrer'>
+                <a href='https://m.facebook.com/photomewp?eav=AfaPVF3S4FdBIcQADqcItT7PaIdqXNWBMLcFx_Mj6O1g7rNdbt-1tTm_KIo9eO1m3tE&paipv=0' target='_blank' rel='noreferrer'>
                     <FontAwesomeIcon icon={faFacebook} className='icon' />
                 </a>
             </section>
